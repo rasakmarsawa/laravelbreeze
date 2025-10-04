9ed643dd43f23768c14dd4ce71a35b085b6c1fba
rasakmarsawa <rasakmarsawa@gmail.com>
2025-10-04 08:55:11 +0700

    make a new middleware

--- routes/web.php
+++ routes/web.php
@@ -4,21 +4,23 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Middleware\isAdminMiddleware;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
-Route::resource('categories', CategoryController::class);
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['verified'])->name('dashboard');
+
+    Route::resource('categories', CategoryController::class)
+    ->middleware(IsAdminMiddleware::class);
+});
 
 require __DIR__.'/auth.php';
